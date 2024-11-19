ab87d2e3f00e5f13041576badb17a165b15d9664
tles-js <tless1477@gmail.com>
2024-11-19 15:08:53 +0700

    first commit

---
+++ surprise-birthday-basic-master/src/assets/mock/mock.js
@@ -0,0 +1,17 @@
+import image1 from "../images/1.png";
+import gift1 from "../images/gifts/1.png";
+
+export const _albums = image1;
+
+export const _giftAlbums = gift1;
+
+export const _messages = [
+    "ลองทำดูเล่นๆให้อ่ะ อาจจะไม่สวยเท่าไหร่เพราะทำไม่ค่อยเก่ง55554564545",
+    "แต่รักนะะ",
+];
+
+export const _birthdayMessages = [
+    "ครบรอบ2เดือนแระ",
+    "ไวจัง แต่ไม่มีไรให้หนุเลยยย",
+    "ไม่ค่อยได้ถ่ายรูปกันเลยเอารูปนี้ไปก่อนน",
+];
